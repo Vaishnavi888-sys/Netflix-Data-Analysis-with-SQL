103ce10573477a20e23f42a1cde1e77272d620b4
Vaishnavi Srinivas <149090670+Vaishnavi888-sys@users.noreply.github.com>
2025-05-27 00:19:06 +0530

    schema.sql

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE netflix(
+	show_id	varchar(10),
+	type varchar(10),
+	title varchar(150),
+	director varchar(255),	
+	casts varchar(1000),
+	country varchar(255),	
+	date_added varchar(50),
+	release_year int,
+	rating	varchar(10),
+	duration varchar(15),
+	listed_in	varchar(100),
+	description varchar(255)
+);
