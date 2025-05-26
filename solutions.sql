00778070e7b11a7d8845fc43b2fb582363fbac1f
Vaishnavi Srinivas <149090670+Vaishnavi888-sys@users.noreply.github.com>
2025-05-27 00:18:02 +0530

    solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,23 +1,3 @@
-
-CREATE TABLE netflix(
-	show_id	varchar(10),
-	type varchar(10),
-	title varchar(150),
-	director varchar(255),	
-	casts varchar(1000),
-	country varchar(255),	
-	date_added varchar(50),
-	release_year int,
-	rating	varchar(10),
-	duration varchar(15),
-	listed_in	varchar(100),
-	description varchar(255)
-);
-SELECT * FROM netflix;
-SELECT DISTINCT type FROM netflix;
-SELECT COUNT(*) AS total_content
-FROM netflix;
-
 --1.count the number of movies vs tv shows
 SELECT type,COUNT(*) AS content
 FROM netflix
